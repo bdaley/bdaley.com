f30014e595fc0061d7842bc09ffbe3ce9db2ec12
Brian Daley <brian.daley@uconn.edu>
2017-03-12 13:15:27 -0400

    updated email address

--- www/mailer/form_setting.php
+++ www/mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from the site bdaley.com"; //letter subject
-	$to = 'brianpdaley@gmail.com'; // Enter Your E-mail
+	$to = 'brian@bdaley.com'; // Enter Your E-mail
 	$from = 'noreply@bdaley.com'; // Admin e-mail
 	$fromName = 'Brian Daley'; // Your company name
 	$charset = 'UTF-8';
